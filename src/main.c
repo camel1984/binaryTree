8a8f824b0cbb8ba217d2659918c5793bd84e84f7
EC2 Default User <ec2-user@ip-172-31-93-147.ec2.internal>
2021-04-03 07:44:23 +0000

    add code

---
+++ src/main.c
@@ -0,0 +1,6 @@
+#include "config.h"
+#include "tree.h"
+
+int main(int argc, char ** argv){
+    f();
+}
---
+++ src/tree.c
@@ -0,0 +1,5 @@
+#include "tree.h"
+#include "stdio.h"
+int f(){
+    printf("this is f\n");
+}
