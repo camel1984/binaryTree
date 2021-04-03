a60148346fa01a7ce75102fe40be95ab7cf132ce
EC2 Default User <ec2-user@ip-172-31-93-147.ec2.internal>
2021-04-03 08:10:27 +0000

    add string.h

--- src/main.c
+++ src/main.c
@@ -1,7 +1,15 @@
 #include "config.h"
 #include "mytree.h"
+#include "stdio.h"
+#include "stdlib.h"
+#include "string.h"
 
 int main(int argc, char ** argv){
     f();
+
+    struct mynode * root = (struct mynode*)malloc(sizeof(struct mynode)); 
+
+    root->data = strdup("node0");
+
     return 0;
 }
